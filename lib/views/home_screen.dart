9c5048adbf4d336a7313491b4b57b896dd83d4dd
Irfan Khan <72300139+irfankhan751@users.noreply.github.com>
2024-11-09 10:23:55 +0500

    Added drawyer to home screen
    Added drawyer to home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:web_dasher/controllers/utills/app_colors.dart';
+import 'package:web_dasher/controllers/utills/app_textStyles.dart';
 
 import 'widgets/carousel_widget.dart';
 import 'widgets/drawer_widget.dart';
@@ -7,6 +9,99 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: AppColors.primaryColor1,
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "X",
+                    style: AppTextStyles().mediumTextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ]),
+            ListTile(
+              leading: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0.5, color: AppColors.greyColor),
+                    color: const Color.fromARGB(255, 244, 203, 82),
+                    borderRadius:
+                        BorderRadius.circular(30.0), // Circular corners
+                  ),
+                  child: Image.asset("assets/pngs/user.png")),
+              title: Text(
+                "Ahmed Chowki",
+                style: AppTextStyles()
+                    .mediumTextStyle(fontSize: 16, color: Colors.white),
+              ),
+              subtitle: Text(
+                "Wallet balance: 0.0 XD",
+                style: AppTextStyles()
+                    .mediumTextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Image.asset("assets/pngs/location.png"),
+                    title: Text(
+                      "Address",
+                      style: AppTextStyles().mediumTextStyle(
+                          fontSize: 16, color: AppColors.greyColor),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/pngs/offer.png"),
+                    title: Text(
+                      "Offers",
+                      style: AppTextStyles().mediumTextStyle(
+                          fontSize: 16, color: AppColors.greyColor),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/pngs/call.png"),
+                    title: Text(
+                      "Support",
+                      style: AppTextStyles().mediumTextStyle(
+                          fontSize: 16, color: AppColors.greyColor),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/pngs/badge.png"),
+                    title: Text(
+                      "Privacy Policy",
+                      style: AppTextStyles().mediumTextStyle(
+                          fontSize: 16, color: AppColors.greyColor),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Image.asset("assets/pngs/wallet.png"),
+                    title: Text(
+                      "English/Arabic",
+                      style: AppTextStyles().mediumTextStyle(
+                          fontSize: 16, color: AppColors.greyColor),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(),
       body: SafeArea(
         child: Container(
           child: Column(
